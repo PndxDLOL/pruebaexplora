2c877768f007c8121c442fd4abcf65b2bb46929a
Juan David Rodrígez <rjuandavid39@gmail.com>
2022-04-26 08:14:02 -0500

    Initial Commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+export default async function handler(req, res, ) {
+
+  try {
+    fetch('https://encasa.parqueexplora.org/wp-json/wp/v2/posts')
+    .then(data => data.json())
+    .then(el => {return{id: el.id}})
+    .then(response => {res.status(200).json(response)})
+    .catch(err => console.log(err));
+
+   
+  }catch(err){
+    console.log(err)
+  }
 }
