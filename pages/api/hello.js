4e08336bd109bbd3c82f328e11b50e4cd6a01e65
Juan David Rodrígez <rjuandavid39@gmail.com>
2022-04-26 22:17:21 -0500

    Prueba fetch para traer datos

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -6,7 +6,7 @@ export default async function handler(req, res, ) {
   try {
     fetch('https://encasa.parqueexplora.org/wp-json/wp/v2/posts')
     .then(data => data.json())
-    .then(el => {return{id: el.id}})
+    .then(el => el.map((exp) => {return{id: exp.id}}))
     .then(response => {res.status(200).json(response)})
     .catch(err => console.log(err));
 
